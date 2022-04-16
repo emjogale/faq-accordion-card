93431b14ff0232998eaac6d83a65efc63085b144
Emma Gale <emmajgale@hotmail.com>
2022-04-16 17:51:29 +0100

    remove extra slashes from begining of js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-// // Grab all the questions
-const questions = document.querySelectorAll(".q-and-a");
+// Grab all the questions
+const questions = document.querySelectorAll(".question");
 
 questions.forEach((question) => {
 	const q = question.querySelector(".question");
 	let btn = question.querySelector("button");
+	console.log(question);
 
 	q.addEventListener("click", () => {
 		questions.forEach((item) => {
 			if (item != q) {
 				item.classList.remove("show");
+				console.log();
 			}
 		});
 
